eab125d3c13ca1a6ab21ba09ab6e4e8d3c82f7d8
odelgadillo <odelgadillo@outlook.com>
2025-02-13 11:11:15 -0400

    Aplicación Web Progresiva (PWA)

--- script.js
+++ script.js
@@ -285,3 +285,15 @@ function setTheme(theme) {
         }
     });
 }
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then((registration) => {
+          console.log('Service Worker registrado con éxito:', registration);
+        })
+        .catch((error) => {
+          console.log('Error al registrar el Service Worker:', error);
+        });
+    });
+  }
