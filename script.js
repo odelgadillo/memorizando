2ecddaf55d88b32af53c8d5441cbaa68c79ca2c5
odelgadillo <odelgadillo@outlook.com>
2025-02-13 11:56:46 -0400

    Ajuste en rutas relativas del service-worker

--- script.js
+++ script.js
@@ -288,7 +288,7 @@ function setTheme(theme) {
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
+      navigator.serviceWorker.register('service-worker.js')
         .then((registration) => {
           console.log('Service Worker registrado con éxito:', registration);
         })
--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,28 @@
 const CACHE_NAME = 'memoletras-v1';
 const ASSETS_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/assets/css/color-modes.css',
-    '/assets/css/cover.css',
-    '/assets/css/estilo.css',
-    '/assets/js/script.js',
-    '/assets/js/color-modes.js',
-    '/assets/img/logo.svg',
-    '/assets/img/favicons/favicon.ico',
-    '/assets/img/favicons/apple-touch-icon.png',
-    '/assets/img/favicons/favicon-96x96.png',
-    '/assets/img/favicons/favicon.svg',
-    '/assets/img/favicons/site.webmanifest'
+    './',
+    './index.html',
+    './assets/css/color-modes.css',
+    './assets/css/cover.css',
+    './assets/css/estilo.css',
+    './assets/js/script.js',
+    './assets/js/color-modes.js',
+    './assets/img/logo.svg',
+    './assets/img/favicons/favicon.ico',
+    './assets/img/favicons/apple-touch-icon.png',
+    './assets/img/favicons/favicon-96x96.png',
+    './assets/img/favicons/favicon.svg',
+    './assets/img/favicons/site.webmanifest'
   ];
 
-self.addEventListener('install', event => {
+  self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then((cache) => {
                 return cache.addAll(ASSETS_TO_CACHE)
+                    .catch((error) => {
+                        console.error('Error al cachear archivos:', error);
+                    });
             })
     );
 });
